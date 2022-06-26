b763e3eefeb2b4c2b328deae54fbdff44b8b52df
ur2e <dbfl9711@gmail.com>
2022-06-26 12:05:22 +0900

    2장) 테스트 코드 작성 HelloController에 Lombok 적용한 HelloResponseDto 추가

---
+++ src/main/java/com/study/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        // 내장 WAS 실행
+        SpringApplication.run(Application.class, args);
+    }
+}
